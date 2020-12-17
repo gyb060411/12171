f4fd379b365790967ad29b0091d824f3568ff740
guopeng00 <gyb@1742784103.com>
2020-12-17 20:48:11 +0800

    第二次

---
+++ app/src/main/java/first/test/a12171/HomeAdapter.java
@@ -0,0 +1,39 @@
+package first.test.a12171;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import first.test.a12171.bean.SchoolBean;
+
+class HomeAdapter extends RecyclerView.Adapter {
+    Context context;
+    private ArrayList<SchoolBean.NewsBean> list;
+    public HomeAdapter(Context context, ArrayList<SchoolBean.NewsBean> list) {
+        this.context=context;
+        this.list=list;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.text_item,parent,false);
+        
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/first/test/a12171/MainActivity.java
+++ app/src/main/java/first/test/a12171/MainActivity.java
@@ -1,14 +1,35 @@
 package first.test.a12171;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import first.test.a12171.bean.SchoolBean;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecycler;
+    private ArrayList<SchoolBean.NewsBean> list;
+    private HomeAdapter homeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mRecycler = findViewById(R.id.recycler);
+        list = new ArrayList<>();
+        homeAdapter = new HomeAdapter(this, list);
+        mRecycler.setLayoutManager(new LinearLayoutManager(this));
+        mRecycler.setAdapter(homeAdapter);
+        mRecycler.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
     }
 }
